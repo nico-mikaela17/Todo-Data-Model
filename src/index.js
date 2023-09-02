cf52f9530f06381a3ece41512776ad3b78ffe5db
nico-mikaela17 <mikaelafs17@gmail.com>
2023-09-01 19:49:11 -0600

    Added tasks

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+
+// initial todos
+
+let todos = [
+  {
+    todoID: 0,
+    todoName: "Finish Homework",
+    todoStatus: "Completed",
+    todoCategory: "School",
+    todoComplete: true,
+    todoDueDate: "Sept 2, 2023",
+  },
+  {
+    todoID: 1,
+    todoName: "Groceries",
+    todoStatus: "Open",
+    todoCategory: "Chores",
+    todoComplete: false,
+    todoDueDate: "Sept 5, 2023",
+  },
+  {
+    todoID: 2,
+    todoName: "Clean the house",
+    todoStatus: "Open",
+    todoCategory: "Chores",
+    todoComplete: false,
+    todoDueDate: "Sept 3, 2023",
+  },
+  {
+    todoID: 3,
+    todoName: "Real Salt Lake game",
+    todoStatus: "Open",
+    todoCategory: "Dates",
+    todoComplete: false,
+    todoDueDate: "Sept 2, 2023",
+  },
+];
